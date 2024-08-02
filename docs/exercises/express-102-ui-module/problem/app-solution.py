f0c632b05d0b5eb4feb9bc82bce956be87bd0c98
Garrett Grolemund <garrett@posit.co>
2024-08-02 16:59:26 -0500

    Updates Exercise 201

---
+++ docs/exercises/express-102-ui-module/problem/app-solution.py
@@ -0,0 +1,15 @@
+from shiny.express import ui
+
+ui.markdown(
+    """
+    ## Why Shiny for Python? 
+
+    Shiny lets you make pure Python web apps quickly, without worrying about: 
+
+    - cache 
+    - state
+    - callbacks 
+
+    ...or even HTML, CSS, and JavaScript.
+    """
+)
