444ec971aa7fb02990fcbb51a7c2d9b4868e3afa
andrie <apdevries@gmail.com>
2024-08-03 07:58:46 +0100

    Additional checks for using shiny express

--- check.py
+++ check.py
@@ -1,8 +1,10 @@
 # import check
-from check import yourturn
-from check import exercises
-from check import quarto_yml
+from checks import yourturn
+from checks import exercises
+from checks import quarto_yml
+from checks import express
 
 yourturn.main()
 exercises.walk_dir()
 quarto_yml.main()
+express.check_express()
