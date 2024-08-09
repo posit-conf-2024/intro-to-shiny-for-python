682c68e60d3ed35853497a514648b3b1547fab43
andrie <apdevries@gmail.com>
2024-08-09 07:35:05 +0100

    Add qrcode extension

--- _extensions/jmbuhr/qrcode/qrcode.lua
+++ _extensions/jmbuhr/qrcode/qrcode.lua
@@ -1,5 +1,5 @@
 -- for development:
-local p = quarto.utils.dump
+local p = quarto.log.warning
 
 ---Format string like in bash or python,
 ---e.g. f('Hello ${one}', {one = 'world'})
@@ -10,14 +10,6 @@ local function f(s, kwargs)
   return (s:gsub('($%b{})', function(w) return kwargs[w:sub(3, -2)] or w end))
 end
 
----Add qrcode js dependencies.
-local function addDependencies()
-  quarto.doc.addHtmlDependency {
-    name = 'qrcodejs',
-    version = 'v1.0.0',
-    scripts = { './assets/qrcode.js' },
-  }
-end
 
 ---Merge user provided options with defaults
 ---@param userOptions table
@@ -44,35 +36,69 @@ local function mergeOptions(url, userOptions)
   return quarto.json.encode(defaultOptions)
 end
 
+
 ---@return string
 local function wrapInlineDiv(options)
   return [[
 <div id="${id}" class="qrcode"></div>
 <script type="text/javascript">
 var qrcode = new QRCode("${id}", ]]
-    .. options..[[);
+      .. options .. [[);
 </script>
+    ]]
+end
+
+---@return string
+local function wrapInlineTex(url, opts)
+  return [[
+\qrcode[]] .. opts .. [[]{]] .. url .. [[}
     ]]
 end
 
 return {
   ['qrcode'] = function(args, kwargs, _)
-    if not quarto.doc.isFormat("html:js") then
-      return pandoc.Null()
+    if quarto.doc.is_format("html:js") then
+      quarto.doc.add_html_dependency {
+        name = 'qrcodejs',
+        version = 'v1.0.0',
+        scripts = { './assets/qrcode.js' },
+      }
+      local url = pandoc.utils.stringify(args[1])
+      local id = 'qrcode'
+      local maybeId = args[2]
+      if maybeId ~= nil then
+        id = pandoc.utils.stringify(maybeId)
+      end
+      local options = mergeOptions(url, kwargs)
+      local text = wrapInlineDiv(options)
+      return pandoc.RawBlock(
+        'html',
+        f(text, { id = id })
+      )
+    elseif quarto.doc.is_format("pdf") then
+      quarto.doc.use_latex_package("qrcode")
+      local url = pandoc.utils.stringify(args[1])
+      local opts = ""
+      for k, v in pairs(kwargs) do
+        if string.match(k, "^pdf") then
+          k = string.sub(k, 4)
+          opts = opts .. k .. "=" .. v .. ", "
+        end
+      end
+      for _, v in ipairs(args) do
+        if string.match(v, "^pdf") then
+          v = string.sub(v, 4)
+          opts = opts .. v .. ", "
+        end
+      end
+      if string.len(opts) then
+        opts = string.sub(opts, 1, string.len(opts) - 2)
+      end
+      local text = wrapInlineTex(url, opts)
+      return pandoc.RawBlock(
+        'tex',
+        text
+      )
     end
-    addDependencies()
-    local url = pandoc.utils.stringify(args[1])
-    local id = 'qrcode'
-    local maybeId = args[2]
-    if maybeId ~= nil then
-      id = pandoc.utils.stringify(maybeId)
-    end
-    local options =mergeOptions(url, kwargs)
-    local text = wrapInlineDiv(options)
-    return pandoc.RawBlock(
-      'html',
-      f(text, {id=id})
-    )
   end,
 }
-
