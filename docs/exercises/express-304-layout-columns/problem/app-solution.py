851499b175ea24fa3b01a995db8a64ff95d609aa
Garrett Grolemund <garrett@posit.co>
2024-08-09 18:00:50 -0500

    Updates UI Theme Slides

--- docs/exercises/express-304-layout-columns/problem/app-solution.py
+++ docs/exercises/express-304-layout-columns/problem/app-solution.py
@@ -1,7 +1,5 @@
 from shiny.express import render, ui, input
-from data_import import df
-from plots import plot_auc_curve, plot_precision_recall_curve, plot_score_distribution
-from shinywidgets import render_plotly
+from shinyswatch import theme
 
 with ui.sidebar():
     ui.input_select(
@@ -16,32 +14,3 @@ with ui.sidebar():
         ],
     )
 
-# `layout_columns` lays child elements out side by side.
-# You can add anything to the layout columns context manager, but it
-# works best with cards and value boxes.
-with ui.layout_columns():
-
-    with ui.card():
-        ui.card_header("Model Metrics")
-
-        @render_plotly
-        def metric_plot():
-            account_subset = df[df["account"] == input.account()]
-            if input.metric() == "ROC Curve":
-                return plot_auc_curve(
-                    account_subset, "is_electronics", "training_score"
-                )
-            else:
-                return plot_precision_recall_curve(
-                    account_subset, "is_electronics", "training_score"
-                )
-
-        ui.input_select("metric", "Metric", choices=["ROC Curve", "Precision Recall"])
-
-    with ui.card():
-        ui.card_header("Model Scores")
-
-        @render_plotly
-        def score_dist():
-            account_subset = df[df["account"] == input.account()]
-            return plot_score_distribution(account_subset)
