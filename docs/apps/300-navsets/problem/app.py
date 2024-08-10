eedb171e58f5b4fd8138751fec1c9cad26e505c6
Garrett Grolemund <garrett@posit.co>
2024-08-10 10:50:03 -0500

    Adds 330-target-app

--- docs/apps/300-navsets/problem/app.py
+++
@@ -1,9 +0,0 @@
-from shiny.express import ui
-
-with ui.navset_tab():
-    with ui.nav_panel( "Panel 1"):
-        ui.input_slider("slider", "Slider", 0, 100, 20)
-    with ui.nav_panel("Panel 2"):
-        ui.input_action_button("button", "Button A")
-    with ui.nav_panel("Panel 3"):
-        ui.input_action_button("button2", "Button B")
